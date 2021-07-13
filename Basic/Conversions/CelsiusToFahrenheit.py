a4ac3a3341c60757595a9fc2f66bdd34b7d45d57
VenegasDEV <contacto@venegas.dev>
2021-07-12 19:17:44 -0500

    Degree conversions

---
+++ Basic/Conversions/CelsiusToFahrenheit.py
@@ -0,0 +1,5 @@
+celsius = float(input('Type the temperature in Celsius degree: '))
+
+fahrenheit = ((celsius * 9) / 5) + 32
+
+print(f'The {celsius} Celsius degree are {fahrenheit} Fahrenheit degrees')
---
+++ Basic/Conversions/FahrenheitToCelsius.py
@@ -0,0 +1,5 @@
+fahrenheit = float(input('Type the temperature in Fahrenheit degree: '))
+
+celsius = ((fahrenheit - 32) * 5) / 9
+
+print(f'The {fahrenheit} Fahrenheit degree are {celsius} in Celsius degree')
