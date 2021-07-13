75454b6c84e8ea0b2013810b14ea53d01daf0390
VenegasDEV <contacto@venegas.dev>
2021-07-12 19:39:30 -0500

    Secret number

--- Basic/Loops/SecretNumber.py
+++ Basic/Loops/SecretNumber.py
@@ -1,6 +1,7 @@
 import random
 
 secretNumber = random.randint(0,10)
+attempts = 0
 flag = False
 
 print(secretNumber)
@@ -11,7 +12,12 @@ while not flag:
     number = int(input('Type the secret number: '))
     if number != secretNumber:
         print(f'{number} is not the secret number')
+        attempts = attempts + 1
     else:
         flag = True
+
+    if attempts == 3:
+        print('I am sorry! you only have three tries')
+        exit(0)
 
 print(f'You are the winner! {number} is the secret number')
