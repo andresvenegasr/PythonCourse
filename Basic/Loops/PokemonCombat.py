b93254f02abbb34780b046f4b636773c76eca6ef
Andres Venegas Ruiz <contacto@venegas.dev>
2021-07-20 21:50:14 -0500

    Updaate pokemon combar

--- Basic/Loops/PokemonCombat.py
+++ Basic/Loops/PokemonCombat.py
@@ -1,31 +1,31 @@
+import os
 import random
 
 life_score_pikachu = 100
 life_score_squirtle = 100
 
-pikachu_attacks = {0: {'attack': 'electro ball', 'damage': 10},
-                   1: {'attack': 'thunderbolt', 'damage': 11}}
+pikachu_attacks = {0: {'attack': 'Electro ball', 'damage': 10},
+                   1: {'attack': 'Thunderbolt', 'damage': 11}}
 
-squirtle_attacks = {0: {'attack': 'tackle', 'damage': 10},
-                    1: {'attack': 'water gun', 'damage': 12},
-                    2: {'attack': 'bubble', 'damage': 9}}
-username = None
-cpu_user = 'CPU'
+squirtle_attacks = {0: {'attack': 'Tackle', 'damage': 10},
+                    1: {'attack': 'Water gun', 'damage': 12},
+                    2: {'attack': 'Bubble', 'damage': 9}}
 
 last_shift = random.randint(0, 1)
-username = input('Enter your username: ')
 
-print(f'The first user to attack is: {username if last_shift == 1 else cpu_user}')
+print('The first user to attack is: {}'.format('Pikachu' if last_shift == 1 else 'Squirtle'))
 
 while life_score_pikachu > 0 and life_score_squirtle > 0:
-    print(f'Pikachu: {life_score_pikachu} Squirtle: {life_score_squirtle}')
+    os.system('cls')
+    # print(f'Pikachu: {life_score_pikachu} Squirtle: {life_score_squirtle}')
+
     if last_shift == 0:
-        print('--------------')
         print('Pikachu turn')
         attack = pikachu_attacks[random.randint(0, 1)]
         print('Pikachu attack with {}'.format(attack['attack']))
         life_score_squirtle = life_score_squirtle - attack['damage']
         last_shift = 1
+
     if last_shift == 1:
         print('--------------')
         print('Squirtle turn')
@@ -40,8 +40,9 @@ while life_score_pikachu > 0 and life_score_squirtle > 0:
         life_score_pikachu = life_score_pikachu - attack['damage']
         last_shift = 0
 
-if life_score_squirtle <= 0:
-    print('CPU is the winner')
+    input("Press key to continue...")
 
-if life_score_pikachu <= 0:
+if life_score_squirtle > life_score_pikachu:
     print('Squirtle is the winner')
+else:
+    print('Pikachu is the winner')
