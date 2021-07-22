f52c39a391a5f90c4eabb49b9ded8b474d701de9
Andres Venegas Ruiz <contacto@venegas.dev>
2021-07-21 22:13:13 -0500

    Update pokemon combat
    - Add life score bars
    - Clear screen in each turn
    - Option to skip the turn
    - Set life score in 0 when is than less to 0

--- Basic/Loops/PokemonCombat.py
+++ Basic/Loops/PokemonCombat.py
@@ -1,24 +1,28 @@
 import os
 import random
 
+# Life score of the Pokemon's
+total_life_score_pokemon = 100;
 life_score_pikachu = 100
 life_score_squirtle = 100
 
+# Collections with attacks per pokemon
 pikachu_attacks = {0: {'attack': 'Electro ball', 'damage': 10},
                    1: {'attack': 'Thunderbolt', 'damage': 11}}
 
-squirtle_attacks = {0: {'attack': 'Tackle', 'damage': 10},
-                    1: {'attack': 'Water gun', 'damage': 12},
-                    2: {'attack': 'Bubble', 'damage': 9}}
+squirtle_attacks = {'T': {'attack': '[T]ackle', 'damage': 10},
+                    'W': {'attack': '[W]ater gun', 'damage': 12},
+                    'B': {'attack': '[B]ubble', 'damage': 9}}
 
 last_shift = random.randint(0, 1)
 
+os.system('cls')
 print('The first user to attack is: {}'.format('Pikachu' if last_shift == 1 else 'Squirtle'))
+input("Press key to continue...")
+os.system('cls')
 
 while life_score_pikachu > 0 and life_score_squirtle > 0:
-    os.system('cls')
-    # print(f'Pikachu: {life_score_pikachu} Squirtle: {life_score_squirtle}')
-
+    # Pikachu turn
     if last_shift == 0:
         print('Pikachu turn')
         attack = pikachu_attacks[random.randint(0, 1)]
@@ -26,21 +30,56 @@ while life_score_pikachu > 0 and life_score_squirtle > 0:
         life_score_squirtle = life_score_squirtle - attack['damage']
         last_shift = 1
 
+        # Show life score of the pokemons
+        # Life bars pikachu
+        life_bars_pikachu = int((life_score_pikachu * 10) / total_life_score_pokemon)
+        print('Pikachu      [{}{}]({}/{})'.format('*' * life_bars_pikachu,
+                                                  ' ' * (10 - life_bars_pikachu),
+                                                  life_score_pikachu if life_score_pikachu > 0 else 0,
+                                                  total_life_score_pokemon))
+
+        # Life bars squirtle
+        life_bars_squirtle = int((life_score_squirtle * 10) / total_life_score_pokemon)
+        print('Squirtle     [{}{}]({}/{})'.format('*' * life_bars_squirtle,
+                                                  ' ' * (10 - life_bars_squirtle),
+                                                  life_score_squirtle if life_score_squirtle > 0 else 0,
+                                                  total_life_score_pokemon))
+
+        input("Press key to continue...")
+        os.system('cls')
+
+    # Squirtle turn
     if last_shift == 1:
-        print('--------------')
         print('Squirtle turn')
         selected_attack = None
 
-        while selected_attack is None:
-            str_selected_attack = input('Select attack [0: tackle, 1:water gun, 2:bubble]: ')
-            selected_attack = int(str(str_selected_attack))
+        while selected_attack not in ['T', 'W', 'B', 'S']:
+            selected_attack = input('Select attack [T]ackle, [W]ater gun, [B]ubble, [S]kip: ')
 
-        attack = squirtle_attacks[selected_attack]
-        print('Squirtle attack with {}'.format(attack['attack']))
-        life_score_pikachu = life_score_pikachu - attack['damage']
+        if selected_attack != 'S':
+            attack = squirtle_attacks[selected_attack]
+            print('Squirtle attack with {}'.format(attack['attack']))
+            life_score_pikachu = life_score_pikachu - attack['damage']
+
         last_shift = 0
 
-    input("Press key to continue...")
+        # Show life score of the pokemons
+        # Life bars pikachu
+        life_bars_pikachu = int((life_score_pikachu * 10) / total_life_score_pokemon)
+        print('Pikachu      [{}{}]({}/{})'.format('*' * life_bars_pikachu,
+                                                  ' ' * (10 - life_bars_pikachu),
+                                                  life_score_pikachu if life_score_pikachu > 0 else 0,
+                                                  total_life_score_pokemon))
+
+        # Life bars squirtle
+        life_bars_squirtle = int((life_score_squirtle * 10) / total_life_score_pokemon)
+        print('Squirtle     [{}{}]({}/{})'.format('*' * life_bars_squirtle,
+                                                  ' ' * (10 - life_bars_squirtle),
+                                                  life_score_squirtle if life_score_squirtle > 0 else 0,
+                                                  total_life_score_pokemon))
+
+        input("Press key to continue...")
+        os.system('cls')
 
 if life_score_squirtle > life_score_pikachu:
     print('Squirtle is the winner')
