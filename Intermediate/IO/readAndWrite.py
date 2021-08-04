813c260d616589c55d6d687d207c71f77682918a
Andres Venegas Ruiz <contacto@venegas.dev>
2021-08-03 20:20:15 -0500

    READ and WRITE file

---
+++ Intermediate/IO/readAndWrite.py
@@ -0,0 +1,49 @@
+import os
+
+EXIT = "EXIT"
+
+accepted_products = ["milk", "chocolate", "eggs", "pen", "chicken"]
+
+
+def add_product():
+    product = None
+    while product not in accepted_products:
+        product = input("Enter a product ({} to close): ".format(EXIT))
+
+        if product == EXIT:
+            break
+
+        if product not in accepted_products:
+            os.system('cls')
+            print("The product is out of stock")
+
+    return product
+
+
+def write_file(filename, shopping_list):
+    file = open(f"{filename}.txt", "w")
+    file.write("\n".join(shopping_list))
+    file.close()
+
+
+def main():
+    os.system('cls')
+    shopping_list = []
+
+    item = add_product()
+
+    while item != EXIT:
+        shopping_list.append(item)
+        print("\n".join(shopping_list))
+        item = add_product()
+        os.system('cls')
+
+    os.system('cls')
+
+    if len(shopping_list) > 0:
+        filename = input("Enter the file name: ")
+        write_file(filename, shopping_list)
+
+
+if __name__ == "__main__":
+    main()
